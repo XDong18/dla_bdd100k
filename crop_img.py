445d73d408ebfb09ef014496d9bed4b54cca75c0
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-18 11:10:27 +0800

    cc

--- crop_img.py
+++ crop_img.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     root_dir = '/shared/xudongliu/code/f_server/dla_bdd100k/vis_out/dla102_up_768x768_500e/dla102up_029_val_color/images/val'
     fn_list = os.listdir(root_dir)
     out_dir = '/shared/xudongliu/code/f_server/dla_bdd100k/vis_out/dla102_up_768x768_500e_new'
+    os.makedirs(out_dir, exist_ok=True)
     for fn in fn_list:
         image_path = os.path.join(root_dir, fn)
         img = Image.open(image_path)
