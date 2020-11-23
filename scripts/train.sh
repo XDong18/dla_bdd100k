716040a2da767527a54542551688e2f6718b6a0d
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-23 20:59:52 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla102up \
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla38up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/dla102_up_768x768_500e
+    --random-color --pretrained-base imagenet -o out/dla38_up_768x768_500e
