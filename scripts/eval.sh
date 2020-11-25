91b1c3c1a55445fea69d5108368fc39da7154131
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-25 10:33:57 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 python3 segment.py train -e -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
