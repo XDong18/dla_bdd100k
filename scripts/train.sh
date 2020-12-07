3105843f6fbb33113a67ea14fec5ae4b9c2af672
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-07 21:15:00 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
-    --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
+    --batch-size 16 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/dla34_up_768x768_500e  --resume out/dla34_up_768x768_500e/checkpoint_164.pth.tar
+    --random-color --pretrained-base imagenet -o out/dla34_up_768x768_16_500e 
