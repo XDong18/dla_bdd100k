91572a05c0b9f59dd46236f4fd54137914fcfb24
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-20 22:08:31 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=4,5,6,7
 python3 segment.py train -e -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/dla34_up_768x768_500e  --resume out/dla34_up_768x768_500e/model_best.pth.tar
+    --random-color --pretrained-base imagenet --resume out/dla34_up_768x768_16_500e/model_best.pth.tar
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
-    --batch-size 16 --lr 0.01 --momentum 0.9 --lr-mode poly \
+export CUDA_VISIBLE_DEVICES=2,3,4,5
+python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla102up \
+    --batch-size 8 --lr 0.005 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/dla34_up_768x768_16_500e 
+    --random-color --pretrained-base imagenet -o out/dla102_up_768x768_bs8_500e 
