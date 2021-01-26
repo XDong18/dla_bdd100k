eb20abc66d08d601d33bd4372de9cc2aba94a75b
XDong18 <liuxd1215@gmail.com>
2021-01-27 04:52:12 +0800

    cc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=2,3,4,5
-python3 segment.py test --phase val -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 1280 --arch dla102up \
+python3 segment.py test --phase val -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 1280 --arch dla34up \
     --batch-size 4 \
     --epochs 500 --bn-sync \
     --with-gt \
-    -e --resume out/dla102_up_768x768_bs8_500e_new_pretrained/model_best.pth.tar
+    -e --resume out/dla34_up_768x768_16_500e/model_best.pth.tar
